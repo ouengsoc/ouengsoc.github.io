d5eed8ecbb45c5934bfb5507454ed1e58091244b
mfmmq <muyiq7@gmail.com>
2016-09-15 23:41:38 +0100

    new mockup

--- script/sponsors.js
+++ script/sponsors.js
@@ -2,18 +2,27 @@
 
 
 
-function toggleGoldContent() {
-    if (document.getElementById('Summary').style.display === 'none') {
-        document.getElementById('Summary').style.display = 'block';
+function toggleGoldContent(SummaryID) {
+    if (document.getElementById(SummaryID).style.display === 'none') {
+        document.getElementById(SummaryID).style.display = 'block';
         return;
     } else {
-        document.getElementById('Summary').style.display = 'none';
+        document.getElementById(SummaryID).style.display = 'none';
         return;
     }
 }
 
 
-
+function toggleOverlay(ID) {
+    if (document.getElementById('Overlay').style.display === 'none') {
+        document.getElementById('Overlay').style.display = 'block';
+        document.getElementById('Fade').style.display = 'block';
+        return;
+    } else {
+        document.getElementById('Overlay').style.display = 'none';      document.getElementById('Fade').style.display = 'none';
+        return;
+    }
+}
 
 
 
