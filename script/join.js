0b03e5debb14e310de602c6c3cbca7739b016ef8
mfmmq <muyiq7@gmail.com>
2016-09-15 04:12:33 +0100

    sponsor, join changes

--- script/join.js
+++ script/join.js
@@ -1,11 +1,21 @@
 'use strict';
 
+var flag_FirstName = 0;
+var flag_LastName = 0;
+var flag_checkbox = 1;
+
 function printEmail(Identity) {
     var Error = 'error' + Identity;
     var Value = document.getElementById(Identity).value;
-    var pattern = new RegExp(/[!$%\^& *()_+|~=`{}\[\]:";'<>?,.\/]/); //unacceptable chars
-    if (pattern.test(Value) || (Value === '')) {
-        document.getElementById(Error).style.visibility = 'visible';
+    var pattern = new RegExp(/[!$%\^&*@()_+|~=`{}\[\]:";'<>?,.\/]/); //unacceptable chars
+    // check if input is valid
+    if (pattern.test(Value) || (Value === '')) { // if unacceptable or no value
+        if (Identity === 'FirstName') {
+            flag_FirstName = 0; // set flag for invalid input
+        } else if (Identity === 'LastName') {
+            flag_LastName = 0;
+        }
+        document.getElementById(Error).style.visibility = 'visible'; // display error message
         if (Value === '') {
             document.getElementById(Error).value = 'error - no value entered [?]';
             return;
@@ -14,17 +24,26 @@ function printEmail(Identity) {
             return;
         }
     }
+    // if function is not returned, move valid output
     document.getElementById(Error).style.visibility = 'hidden'; //good input
     var FirstName = document.getElementById('FirstName').value;
     var LastName = document.getElementById('LastName').value;
     var College = document.getElementById('College').value;
-    if (FirstName == '') {
+    // set flag for valid input and output
+    if (Identity === 'FirstName') {
+        flag_FirstName = 1; // set flag for invalid input
+    } else if (Identity === 'LastName') {
+        flag_LastName = 1;
+    }
+    if (FirstName === '') {
         FirstName = 'First';
     }
-    if (LastName == '') {
+    if (LastName === '') {
         LastName = 'Last';
     }
-    document.getElementById('Email').value = FirstName + '.' + LastName + '@' + College + '.ox.ac.uk';
+    var FullEmail = FirstName + '.' + LastName + '@' + College + '.ox.ac.uk';
+    document.getElementById('Email').value = FullEmail;
+    document.getElementById('nextPage').value = '/sub/thanks.html#email=' + FullEmail;
 }
 
 
@@ -32,14 +51,15 @@ function printCollege() {
     var FirstName = document.getElementById('FirstName').value;
     var LastName = document.getElementById('LastName').value;
     var College = document.getElementById('College').value;
-    if (FirstName == '') {
+    if (FirstName === '') {
         FirstName = 'First';
     }
-    if (LastName == '') {
+    if (LastName === '') {
         LastName = 'Last';
     }
-    document.getElementById('Email').value = FirstName + '.' + LastName + '@' + College + '.ox.ac.uk';
-    
+    var FullEmail = FirstName + '.' + LastName + '@' + College + '.ox.ac.uk';
+    document.getElementById('Email').value = FullEmail;
+    document.getElementById('nextPage').value = '/sub/thanks.html#email=' + FullEmail;    
 }
 
 
@@ -52,3 +72,29 @@ function toggleAlert(Identity) {
         return;
     }
 }
+
+
+function toggleCheckbox() {
+    var check_Membership = document.getElementById('RegisterMembership').checked;
+    var check_Mailing = document.getElementById('RegisterMailing').checked;
+    if ((check_Mailing === false) && (check_Membership === false)) {
+        document.getElementById('Error_Submit_Message').innerHTML = 'error - missing value [?]';
+    } else {
+        document.getElementById('Error_Submit_Message').innerHTML = '';
+    }
+}
+
+
+
+function validateForm() {
+    //check if flags are okay
+}
+
+
+
+
+
+
+
+
+
---
+++ script/sponsors.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+
+function toggleGoldContent() {
+    if (document.getElementById('Summary').style.display === 'none') {
+        document.getElementById('Summary').style.display = 'block';
+        return;
+    } else {
+        document.getElementById('Summary').style.display = 'none';
+        return;
+    }
+}
+
+
+
+
+
+
+
+
